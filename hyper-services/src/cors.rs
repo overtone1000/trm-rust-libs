a0f6918300ed16e1f4543a4df9981320443f2027
Tyler Moore <TylerRMoore@gmail.com>
2024-10-17 08:32:46 -0700

    Better for syntax to use owned values

--- hyper-services/src/cors.rs
+++ hyper-services/src/cors.rs
@@ -4,21 +4,24 @@ pub const HEADER_ORIGIN: &str = "Origin";
 const HEADER_ACCESS_CONTROL_ALLOW_ORIGIN: &str = "Access-Control-Allow-Origin";
 
 type Allowed_Origin_Function<T, U> = dyn Fn(&Request<T>, &hyper::Response<U>) -> String;
+
 pub fn set_cors_allowed_origins<T, U>(
     request: &Request<T>,
-    response: &mut Response<U>,
+    mut response: Response<U>,
     allowed_origins: &Allowed_Origin_Function<T, U>,
-) {
-    let valstr = allowed_origins(request, response);
+) -> Response<U> {
+    let valstr = allowed_origins(request, &response);
     let value: HeaderValue = HeaderValue::from_str(&valstr).expect("Should be a valid header.");
     response
         .headers_mut()
         .insert(HEADER_ACCESS_CONTROL_ALLOW_ORIGIN, value);
+    response
 }
 
-pub fn permit_all_cors<T>(response: &mut Response<T>) {
+pub fn permit_all_cors<T>(mut response: Response<T>) -> Response<T> {
     let value: HeaderValue = HeaderValue::from_str("*").expect("Should be a valid header.");
     response
         .headers_mut()
         .insert(HEADER_ACCESS_CONTROL_ALLOW_ORIGIN, value);
+    response
 }
