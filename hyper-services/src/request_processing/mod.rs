452dc3c226587393affec98feaaa5a8d773bcdcc
Tyler Moore <TylerRMoore@gmail.com>
2024-10-28 09:30:02 -0700

    Only use incoming as arg

--- hyper-services/src/request_processing/mod.rs
+++ hyper-services/src/request_processing/mod.rs
@@ -1,11 +1,10 @@
+use futures_util::StreamExt;
 use http_body_util::BodyExt;
 use hyper::{body::Incoming, Request};
 
 use crate::commons::HandlerError;
 
-pub async fn get_request_body_as_string(
-    request: Request<Incoming>,
-) -> Result<String, HandlerError> {
+pub async fn get_request_body_as_string(request: Incoming) -> Result<String, HandlerError> {
     let collected_request = request.collect().await?.to_bytes().to_vec();
     let parsed_request = String::from_utf8(collected_request)?;
     Ok(parsed_request)
