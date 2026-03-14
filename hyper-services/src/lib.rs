ac4a70e7d85462d003bbc990a8a11b20b9d2a3f5
Tyler <tylerrmoore@gmail.com>
2026-03-14 15:12:03 -0700

    tls

--- hyper-services/src/lib.rs
+++ hyper-services/src/lib.rs
@@ -9,6 +9,9 @@ use hyper_util::rt::{TokioIo, TokioTimer};
 
 use service::stateless_service::{StatelessHandler, StatelessService};
 use tokio::net::TcpListener;
+use tokio_rustls::{TlsAcceptor, rustls::ServerConfig};
+
+use tokio_rustls::rustls::pki_types::{CertificateDer, PrivateKeyDer};
 
 pub mod commons;
 pub mod cors;
@@ -17,16 +20,24 @@ pub mod request_processing;
 pub mod response_building;
 pub mod service;
 
+pub struct TlsCerts
+{
+    pub certs:Vec<CertificateDer<'static>>,
+    pub keys:PrivateKeyDer<'static>
+}
+
 pub struct ConnectionProperties
 {
-    pub with_upgrades:bool
+    pub with_upgrades:bool,
+    pub tls:Option<TlsCerts>
 }
 
 impl Default for ConnectionProperties
 {
     fn default() -> Self {
         Self { 
-            with_upgrades: false
+            with_upgrades: false,
+            tls: None
         }
     }
 }
@@ -64,37 +75,99 @@ where
         }
     }
 
+    let tls_handler = match props.tls
+    {
+        Some(certs)=>{
+             // Load public certificate.
+                        
+            let mut server_config = match ServerConfig::builder()
+                .with_no_client_auth()
+                .with_single_cert(certs.certs, certs.keys)
+                .map_err(|e|  std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))
+                {
+                    Ok(config)=>config,
+                    Err(e)=>{
+                        eprintln!("Couldn't initialize tls handler");
+                        return Err(Box::new(e));
+                    }
+                };
+            server_config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec(), b"http/1.0".to_vec()];
+            let tls_acceptor = TlsAcceptor::from(std::sync::Arc::new(server_config));
+            Some(tls_acceptor)
+        }
+        None=>None
+    };
+
     println!("Starting listen loop on {}:{}", ip, port);
     loop {
         match listener.accept().await {
             Ok((tcp, _)) => {
-                //Need to spawn these as separate tasks...
-                let io = TokioIo::new(tcp);
+                
                 let clone = service.clone();
 
-                let connection: http1::Connection<TokioIo<tokio::net::TcpStream>, S>=http1::Builder::new()
-                        .timer(TokioTimer::new())
-                        .serve_connection(io, clone);
+                match &tls_handler
+                {
+                    Some(tls)=>{
+                        let tls = tls.clone();
 
+                        tokio::spawn(async move {
+                            let tls_stream = match tls.accept(tcp).await {
+                                Ok(tls_stream) => tls_stream,
+                                Err(err) => {
+                                    eprintln!("failed to perform tls handshake: {err:#}");
+                                    return;
+                                }
+                            };
 
+                            service_connection(tls_stream, clone, props.with_upgrades).await
+                        });
+                    },
+                    None=>{
+                        service_connection(tcp, clone, props.with_upgrades).await
+                    }
+                }
+
+                /*
+                //Old pattern was to spawn handler. Necessary still?
                 match props.with_upgrades
                 {
                     true=>tokio::task::spawn(async move {handle_result(connection.with_upgrades().await)}),
                     false=>tokio::task::spawn(async move {handle_result(connection.await)})
-                };
-
-                //Can't await, must be handled in spawned task!
-                //match result.await
-                //{
-                //    Ok(_)=>(),
-                //    Err(e)=>println!("Error serving connection: {:?}", e)
-                //};
+                };     
+                */
             }
             Err(_) => {
                 eprintln!("Couldn't accept tcp, retrying.")
             }
         };
     }
+}
+
+async fn service_connection<StreamType,S>(stream:StreamType, service_clone:S, with_upgrades:bool)->()
+where
+    S: 'static + Clone + Send + HttpService<Incoming>,
+    S::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    S::ResBody: 'static,
+    <S::ResBody as Body>::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    <S as HttpService<hyper::body::Incoming>>::ResBody: Send,
+    <S as HttpService<hyper::body::Incoming>>::Future: Send,
+    <<S as HttpService<hyper::body::Incoming>>::ResBody as hyper::body::Body>::Data: Send,
+    StreamType: 'static + tokio::io::AsyncRead+tokio::io::AsyncWrite+std::marker::Unpin+std::marker::Send
+{
+    tokio::spawn(async move {
+            let io = TokioIo::new(stream);
+
+            let connection=http1::Builder::new()
+                .timer(TokioTimer::new())
+                .serve_connection(io, service_clone);
+
+            match with_upgrades
+            {   
+                true=>handle_result(connection.with_upgrades().await),
+                false=>handle_result(connection.await)
+            };
+        }
+    );
 }
 
 fn handle_result<T:std::error::Error>(result:Result<(),T>)->()
